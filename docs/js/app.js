dc33717a5bae32c6f9154ae65b8bc227d34f0be3
fushikky <fushikky@gmail.com>
2022-01-01 01:03:45 +0900

    doc

--- docs/js/app.js
+++ docs/js/app.js
@@ -33,9 +33,9 @@ AFRAME.registerComponent('markercontroller', {
 
         var posAry = position.toArray();
         posAry = [
-            posAry[0] + 250,
+            posAry[0],
             posAry[1],
-            posAry[2] + 250,
+            posAry[2],
         ];
         var rotAry = rotation.toArray();
         var a = scale.toArray();
